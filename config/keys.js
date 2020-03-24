5195a5ee22b90bcdd50bad2cd79e1087053ced43
Shurui Liu <shurui91@gmail.com>
2020-03-24 16:14:59 -0700

    lecture 23, keys file added

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+	googleClientID: '713354894804-rm40h5rl6bagvjg1r14oc74kmpqj0qnr.apps.googleusercontent.com',
+	googleClientSecret: 'SdQdM0Dl_ekwS2qGWxEVpQL7'
+}
--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 // the way server-side code to get library files
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app = express();
 
-// route handler
-app.get('/', (req, res) => {
-	res.send({ hi: 'there' });
-});
+// console.developers.google.com
+// 713354894804-rm40h5rl6bagvjg1r14oc74kmpqj0qnr.apps.googleusercontent.com
+// SdQdM0Dl_ekwS2qGWxEVpQL7
+passport.use(new GoogleStrategy());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
