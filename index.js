18b01756ed00c62dd6a1d375e92354a25e498bf3
Shurui Liu <shurui91@gmail.com>
2020-03-24 17:04:35 -0700

    lecture 25

--- index.js
+++ index.js
@@ -2,12 +2,33 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 const app = express();
 
 // console.developers.google.com
 // 713354894804-rm40h5rl6bagvjg1r14oc74kmpqj0qnr.apps.googleusercontent.com
 // SdQdM0Dl_ekwS2qGWxEVpQL7
-passport.use(new GoogleStrategy());
+passport.use(
+	new GoogleStrategy(
+		{
+			clientID: keys.googleClientID,
+			clientSecret: keys.googleClientSecret,
+			callbackURL: '/auth/google/callback'
+		},
+		accessToken => {
+			console.log(accessToken);
+		}
+	)
+);
+
+app.get(
+	// path
+	'/auth/google',
+	passport.authenticate('google', {
+		// what kind of access is given
+		scope: ['profile', 'email']
+	})
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
