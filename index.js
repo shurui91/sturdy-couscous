b51bc4fc0743ab658fc2c930d2af6945e93c9cff
Aaron Liu <shurui91@gmail.com>
2020-03-25 18:56:49 -0700

    lecture 26 is done

--- index.js
+++ index.js
@@ -29,5 +29,11 @@ app.get(
 	})
 );
 
+// https://accounts.google.com/o/oauth2/v2/auth?response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Fauth%2Fgoogle%2Fcallback&scope=profile%20email&client_id=713354894804-rm40h5rl6bagvjg1r14oc74kmpqj0qnr.apps.googleusercontent.com
+
+// https://console.developers.google.com/apis/credentials/oauthclient/713354894804-rm40h5rl6bagvjg1r14oc74kmpqj0qnr.apps.googleusercontent.com?project=713354894804
+
+app.get('auth/google/callback', passport.authenticate('google'));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
