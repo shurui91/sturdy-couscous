d68c0ceadacccdb8bcc23503345ba332a4da1ff8
Aaron Liu <shurui91@gmail.com>
2020-03-26 21:54:37 -0700

    lecture 35 is done

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,8 @@
 module.exports = {
-	googleClientID: '713354894804-rm40h5rl6bagvjg1r14oc74kmpqj0qnr.apps.googleusercontent.com',
-	googleClientSecret: 'SdQdM0Dl_ekwS2qGWxEVpQL7'
-}
+	googleClientID:
+		'713354894804-rm40h5rl6bagvjg1r14oc74kmpqj0qnr.apps.googleusercontent.com',
+	googleClientSecret: 'SdQdM0Dl_ekwS2qGWxEVpQL7',
+	mongoURI:
+		'mongodb+srv://dbUser:zF7xZF1m1IOvdzQrM@emaily-jqu1e.mongodb.net/test?retryWrites=true&w=majority',
+	cookieKey: 'redacted'
+};
--- index.js
+++ index.js
@@ -15,3 +15,5 @@ app;
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
+mongoose.connect(keys.mongoURI);
